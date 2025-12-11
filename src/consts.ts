063cb850de1821daca7ddde80651d1438d316827
Moulders26 <matt@emould.net>
2025-12-11 20:57:03 +0000

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'emould.net';
+export const SITE_TITLE = 'eMould';
 export const SITE_DESCRIPTION = 'Personal family domain of Matt Mould – IT, homelab, DIY & tinkering notes.';
